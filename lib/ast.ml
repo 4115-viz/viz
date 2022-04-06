f1ef094c17e229836484878a526bd5ce6865607e
Matthew Duran <mduran2429@gmail.com>
2022-04-06 14:25:34 -0400

    refactored ast and sast
    changed the print functions (huge deal for the vc.ml compilation).
    
    The print statements follow microC naming conventions, but for the
    most part look like what we have been used to prior.
    
    Someone should pick this up and make them look liek what we had.
    The readability was nice.

--- lib/ast.ml
+++ lib/ast.ml
@@ -10,32 +10,29 @@ type builtin_type =
   | BoolType
   | FloatType
 
-  type expr =
+type expr =
   | Assign of string * expr
+  | StrLit of string
   | IntLit of int
   | FloatLit of float
-  | StrLit of string
   | BoolLit of bool
   | Id of string
   | FuncCall of string * expr list
   | Binop of expr * bop * expr
   | Unop of uop * expr
 
-type bind = builtin_type * string
-
-(* ----- Entry ----- *)
-type program = bind list * func_decl list
-
-and stmt =
+type stmt =
   | Expr of expr
   | Block of stmt list
   | If of expr * stmt * stmt
-  | While of expr * stmt
+  (*| While of expr * stmt *)
   | Return of expr
-  | VarDecl of bind * expr option
-  | FuncDecl of func_decl
+  (*| VarDecl of bind * expr option *)
+  (*| FuncDecl of func_decl*) (* don't think this applies anymore *)
 
-and func_decl = {
+type bind = builtin_type * string
+
+type func_def = {
   typ: builtin_type;
   name: string;
   params: bind list;
@@ -43,82 +40,110 @@ and func_decl = {
   body: stmt list;
 }
 
+(* ----- Entry ----- *)
+type program = bind list * func_def list
+
 (* ----- Print Function ----- *)
-let fmt_typ = function
-  | NoneType -> "Type(None)"
-  | StrType -> "Type(Str)"
-  | IntType -> "Type(Int)"
-  | BoolType -> "Type(Bool)"
-  | FloatType -> "Type(Float)"
-
-let fmt_op = function
-| Add -> "+"
-| Sub -> "-"
-| Mult -> "*"
-| Div -> "/"
-| Mod -> "%"
-| Eq -> "=="
-| Neq -> "!="
-| Less -> "<"
+let string_of_op = function
+| Add   -> "+"
+| Sub   -> "-"
+| Mult  -> "*"
+| Div   -> "/"
+| Mod   -> "%"
+| Eq    -> "=="
+| Neq   -> "!="
+| Less  -> "<"
 | Great -> ">"
-| Leq -> "<="
-| Geq -> ">="
-| And -> "and"
-| Or  -> "or"
+| Leq   -> "<="
+| Geq   -> ">="
+| And   -> "and"
+| Or    -> "or"
 
-let fmt_uop = function
+let string_of_uop = function
 | Not -> "not"
 
 let fmt_string x = String.concat "" ["\""; x; "\""]
 
-let rec fmt_fcall name args = 
+let rec string_of_expr = function
+  | StrLit(x) -> "StrLit(" ^ fmt_string x ^ ")"
+  | IntLit(x) -> "IntLit(" ^ string_of_int x ^ ")"
+  | FloatLit(x) -> "FloatLit(" ^ string_of_float x ^ ")"
+  | BoolLit(true) -> "BoolLit(true)"
+  | BoolLit(false) -> "BoolLit(false)"
+  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+  | Id(x) -> "Id(" ^ x ^ ")"
+  | FuncCall(name, args) ->
+    (*name ^ "(" ^ String.concat ", " (List.map string_of_expr args) ^ ")"*)
+    fmt_fcall name args
+  | Binop(l, bo, r) ->
+    string_of_expr l ^ " " ^ string_of_op bo ^ " " ^ string_of_expr r
+  | Unop(uo, r) ->
+    string_of_uop uo ^ " " ^ string_of_expr r
+
+and string_of_stmt = function
+
+  | Expr e -> "  " ^ string_of_expr e
+  | Block (stmts) ->
+    "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Return (expr) -> "return " ^ string_of_expr expr ^ ";\n" 
+  | If (e, s1, s2) -> "if (" ^ string_of_expr e ^ ")\n" ^
+                        string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  (*| VarDecl (b, e) -> fmt_vdecl (b, e)*)
+  (* | FuncDecl fd -> fmt_fdecl fd *) (* don't think this applies anymore *)
+  (*| While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s*)
+
+and fmt_fcall name args = 
   "FuncCall(" ^
      "name: " ^ fmt_string name ^
      ", args: " ^ fmt_expr_list args ^
   ")"
+and fmt_expr_list l = String.concat "\n" (List.map string_of_expr l)
 
-and fmt_expr_list l = String.concat "\n" (List.map fmt_expr l)
+let string_of_typ = function
+  | NoneType  -> "Type(None)"
+  | StrType   -> "Type(Str)"
+  | IntType   -> "Type(Int)"
+  | BoolType  -> "Type(Bool)"
+  | FloatType -> "Type(Float)"
 
-and fmt_expr e = 
-  "(" ^ 
-  (match e with 
-  | StrLit(x) -> "StrLit(" ^ fmt_string x ^ ")"
-  | IntLit(x) -> "IntLit(" ^ fmt_string (string_of_int x) ^ ")"
-  | FloatLit(x) -> "FloatLit(" ^ fmt_string (string_of_float x) ^ ")"
-  | BoolLit(true) -> "BoolLit(true)"
-  | BoolLit(false) -> "BoolLit(false)"
-  | Assign(v, e) -> v ^ " = " ^ fmt_expr e
-  | Id(x) -> "Id(" ^ x ^ ")"
-  | FuncCall(name, args) -> fmt_fcall name args
-  | Binop(l, bo, r) ->
-    fmt_expr l ^ " " ^ fmt_op bo ^ " " ^ fmt_expr r
-  | Unop(uo, r) ->
-    fmt_uop uo ^ " " ^ fmt_expr r
-  )
-  ^ ")"
-
-let fmt_vdecl ((t, n), e) =
+(* let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n" *)
+(*let string_of_vdecl ((t, n), e) = *)
+let string_of_vdecl (t, n) =
   "VarDecl(" ^
     "name: " ^ fmt_string n ^
-    ", type: " ^ fmt_typ t ^
+    ", type: " ^ string_of_typ t ^"\n"(* ^
     match e with
     | None -> ""
-    | Some(e) -> ", value: " ^ fmt_expr e
+    | Some(e) -> ", value: " ^ string_of_expr e *)
 
-let rec fmt_fdecl fd =
-  "Function(" ^ 
-      "name: " ^ fmt_string fd.name ^
-      ", type: " ^ fmt_typ fd.typ ^
-    ")" ^ " {\n\t" ^
-      fmt_stmt_list ~sp:"\n\t" fd.body
-    ^
-    "\n}\n"
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.typ ^ " " ^
+  fdecl.name ^ "(" ^ String.concat ", " (List.map snd fdecl.params) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
 
-and fmt_stmt = function
-  | Expr e -> "  " ^ fmt_expr e
-  | VarDecl (b, e) -> fmt_vdecl (b, e)
-  | FuncDecl fd -> fmt_fdecl fd
+(*let rec fmt_fdecl fd =
+    "Function(" ^ 
+        "name: " ^ fmt_string fd.name ^
+        ", type: " ^ string_of_typ fd.typ ^
+      ")" ^ " {\n\t" ^
+        fmt_stmt_list ~sp:"\n\t" fd.body
+      ^
+      "\n}\n"
+  
+and fmt_stmt_list ?(sp = "\n") l = String.concat sp (List.map string_of_stmt l)
+*)
 
-and fmt_stmt_list ?(sp = "\n") l = String.concat sp (List.map fmt_stmt l)
+let string_of_program (vars, funcs) =
+  "\n\nParsed program: \n\n" ^
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
 
-let string_of_program p = fmt_stmt_list p
+(* let string_of_program p = fmt_stmt_list p *)
+(*let string_of_program (vars, funcs) =
+  "\n\nParsed program: \n\n" ^
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map fmt_fdecl funcs)
+*)
--- lib/sast.ml
+++ lib/sast.ml
@@ -12,94 +12,102 @@ and sx =
   | SBinop of sexpr * bop * sexpr
   | SUnop of uop * sexpr
 
-  type sstmt =
+type sstmt =
   | SExpr of sexpr
   | SBlock of sstmt list
   | SIf of sexpr * sstmt * sstmt
-  | SWhile of sexpr * sstmt
+  (* | SWhile of sexpr * sstmt *)
   | SReturn of sexpr
-  | SVarDecl of bind * sexpr option
-  | SFuncDecl of sfunc_decl
+  (*| SVarDecl of bind * sexpr option*)
+  (*| SFuncDecl of sfunc_decl*) (* don't think this applies anymore *)
 
-and sfunc_decl = {
+and sfunc_def = {
   styp: builtin_type;
   sname: string;
   sparams: bind list;
   sbody: sstmt list;
-  locals: bind list;
+  slocals: bind list;
 }
 
-type sprogram = bind list * sfunc_decl list
+type sprogram = bind list * sfunc_def list
 
 (* ----- Print Function ----- *)
-let fmt_typ = function
-  | NoneType -> "Type(None)"
-  | StrType -> "Type(Str)"
-  | IntType -> "Type(Int)"
-  | FloatType -> "Type(Float)"
-  | BoolType -> "Type(Bool)"
+let rec string_of_sexpr (t, se) =
+  "(" ^ string_of_typ t ^ " : " ^ 
+  (match se with
+    | SStrLit(x) -> "StrLit(" ^ fmt_string x ^ ")"
+    | SIntLit(x) -> "IntLit(" ^ string_of_int x ^ ")"
+    | SFloatLit(x) -> "FloatLit(" ^ string_of_float x ^ ")"
+    | SBoolLit(true) -> "BoolLit(true)"
+    | SBoolLit(false) -> "BoolLit(false)"
+    | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
+    | SId(x) -> "Id(" ^ x ^ ")"
+    | SFuncCall(name, args) -> fmt_sfcall name args
+    | SBinop(l, bo, r) -> 
+      string_of_sexpr l ^ " " ^ string_of_op bo ^ " " ^ string_of_sexpr r 
+    | SUnop(uo, r) ->
+        string_of_uop uo ^ " " ^ string_of_sexpr r
+  )
+  ^ ")"
 
+and string_of_sstmt = function
+  | SExpr se -> "  " ^ string_of_sexpr se
+  | SBlock (sstmts) ->
+    "{\n" ^ String.concat "" (List.map string_of_sstmt sstmts) ^ "}\n"
+  | SReturn (sexpr) -> "return " ^ string_of_sexpr sexpr ^ ";\n" 
+  | SIf (se, s1, s2) -> "if (" ^ string_of_sexpr se ^ ")\n" ^
+      string_of_sstmt s1 ^ "else\n" ^ string_of_sstmt s2
+  (*| SVarDecl (_, _) -> "SVarDecl TODO"*)
+  (*| SFuncDecl sfd -> fmt_sfdecl sfd *) (* don't think this applies anymore *)
+  (*| While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s *)
 
-let fmt_string x = String.concat "" ["\""; x; "\""]
-
-let fmt_op = function
-| Add -> "+"
-| Sub -> "-"
-| Mult -> "*"
-| Div -> "/"
-| Mod -> "%"
-| Eq -> "=="
-| Neq -> "!="
-| Less -> "<"
-| Great -> ">"
-| Leq -> "<="
-| Geq -> ">="
-| And -> "and"
-| Or  -> "or"
-
-let fmt_uop = function
-| Not -> "not"
-
-let rec fmt_sfcall name args = 
+and fmt_sfcall name args = 
   "FuncCall(" ^
      "name: " ^ fmt_string name ^
      ", args: " ^ fmt_sexpr_list args ^
   ")"
 
-and fmt_sexpr_list l = String.concat "\n" (List.map fmt_sexpr l)
+and fmt_sexpr_list l = String.concat "\n" (List.map string_of_sexpr l)
 
-and fmt_sexpr (t, se) =
-  "(" ^ fmt_typ t ^ " : " ^ 
-  (match se with
-    | SStrLit(x) -> "StrLit(" ^ fmt_string x ^ ")"
-    | SIntLit(x) -> "IntLit(" ^ fmt_string (string_of_int x) ^ ")"
-    | SFloatLit(x) -> "FloatLit(" ^ fmt_string (string_of_float x) ^ ")"
-    | SBoolLit(true) -> "BoolLit(true)"
-    | SBoolLit(false) -> "BoolLit(false)"
-    | SAssign(v, e) -> v ^ " = " ^ fmt_sexpr e
-    | SId(x) -> "Id(" ^ x ^ ")"
-    | SFuncCall(name, args) -> fmt_sfcall name args
-    | SBinop(l, bo, r) -> 
-      fmt_sexpr l ^ " " ^ fmt_op bo ^ " " ^ fmt_sexpr r 
-    | SUnop(uo, r) ->
-        fmt_uop uo ^ " " ^ fmt_sexpr r
-  )
-  ^ ")"
 
-let rec fmt_sfdecl sfd = 
-  "Function(" ^ 
-    "name: " ^ fmt_string sfd.sname ^
-    ", type: " ^ fmt_typ sfd.styp ^
-  ")" ^ " {\n" ^
-    fmt_sstmt_list sfd.sbody
-  ^
-  "\n}\n"
+(* let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n" *)
+(*let string_of_vdecl ((t, n), e) = *)
+let string_of_svdecl (t, n) =
+  "VarDecl(" ^
+    "name: " ^ fmt_string n ^
+    ", type: " ^ string_of_typ t ^"\n"(* ^
+    match e with
+    | None -> ""
+    | Some(e) -> ", value: " ^ string_of_expr e *)
 
-and fmt_sstmt = function
-  | SExpr se -> "  " ^ fmt_sexpr se
-  | SVarDecl (_, _) -> "SVarDecl TODO"
-  | SFuncDecl sfd -> fmt_sfdecl sfd
+let string_of_sfdecl (fdecl:sfunc_def) =
+  string_of_typ fdecl.styp ^ " " ^
+  fdecl.sname ^ "(" ^ String.concat ", " (List.map snd fdecl.sparams) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_svdecl fdecl.slocals) ^
+  String.concat "" (List.map string_of_sstmt fdecl.sbody) ^
+  "}\n"
 
-and fmt_sstmt_list l = String.concat "\n" (List.map fmt_sstmt l)
+(*let rec fmt_fdecl fd =
+    "Function(" ^ 
+        "name: " ^ fmt_string fd.name ^
+        ", type: " ^ string_of_typ fd.typ ^
+      ")" ^ " {\n\t" ^
+        fmt_stmt_list ~sp:"\n\t" fd.body
+      ^
+      "\n}\n"
+  
+and fmt_stmt_list ?(sp = "\n") l = String.concat sp (List.map string_of_stmt l)
+*)
 
-let string_of_sprogram sp = fmt_sstmt_list sp
+let string_of_sprogram (vars, funcs) =
+  "\n\nSemantically checked program: \n\n" ^
+  String.concat "" (List.map string_of_svdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_sfdecl funcs)
+
+(* let string_of_program p = fmt_stmt_list p *)
+(*let string_of_program (vars, funcs) =
+  "\n\nParsed program: \n\n" ^
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map fmt_fdecl funcs)
+*)
