9b69657fc20fe91f29f571012e264b8fd0885454
Yanhao Li <hi@yanhaoli.com>
2022-04-07 19:24:12 -0400

    feat: added llvm module to project

--- bin/vc.ml
+++ bin/vc.ml
@@ -1,7 +1,7 @@
 (* Viz Compiler *)
 open Lib
 
-type action = Ast | Sast | Compile | ScanTest
+type action = Ast | Sast | Compile | ScanTest | LLVM_IR
 
 let () = 
 	let action = ref Compile in
@@ -9,31 +9,30 @@ let () =
 	let speclist = [
 		("-a", Arg.Unit (set_action Ast), "Print the AST");
 		("-s", Arg.Unit (set_action Sast), "Print the SAST");
+    ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
 		("-ts", Arg.Unit (set_action ScanTest), "Print the Scanned Tokens");
 	] in  
 	let usage_msg = "usage: vc [-a|-s|-ts] <file.viz>" in
 	let channel = ref stdin in
 	Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
 
+	let lexbuf = Lexing.from_channel !channel in
+	let ast = Parser.program Scanner.token lexbuf in
 	match !action with
-	| ScanTest -> 
-		let lexbuf = Lexing.from_channel !channel in
-		print_endline (Token_fmt.string_of_lexbuf lexbuf)
-	| Ast ->
-		let lexbuf = Lexing.from_channel !channel in
-		let ast = Parser.program Scanner.token lexbuf in
-		print_endline (Ast_fmt.string_of_program ast)
-	| Sast -> 
-		let lexbuf = Lexing.from_channel !channel in
-		let ast = Parser.program Scanner.token lexbuf in
-		let sast = Semant.check ast in
-		print_endline  (Sast_fmt.string_of_sprogram sast)
-	| Compile -> 
-		let lexbuf = Lexing.from_channel !channel in
-		let ast = Parser.program Scanner.token lexbuf in
-		 (* _ in front of name allows us to suppress the fact we are
-				not using the _sast variable currently.
-		 *)
-		let _sast = Semant.check ast in
-		()
-		
+	| ScanTest -> print_endline (Token_fmt.string_of_lexbuf lexbuf)
+	| Ast -> print_endline (Ast_fmt.string_of_program ast)
+	| _ -> let sast = Semant.check ast in
+		match !action with
+			Ast -> ()
+		| ScanTest -> ()
+		| Sast -> print_endline  (Sast_fmt.string_of_sprogram sast)
+		| LLVM_IR -> print_string (Llvm.string_of_llmodule (Irgen.translate sast))
+		| Compile -> 
+			let lexbuf = Lexing.from_channel !channel in
+			let ast = Parser.program Scanner.token lexbuf in
+			(* _ in front of name allows us to suppress the fact we are
+					not using the _sast variable currently.
+			*)
+			let _sast = Semant.check ast in
+			()
+			
