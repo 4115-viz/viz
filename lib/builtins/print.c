787b93fd275da27f38a714449d460787349300b8
Matthew Duran <mduran2429@gmail.com>
2022-04-11 11:27:03 -0400

    added other print functions to c lib
    commented out though, Idk how yanhao wants to approach this

--- lib/builtins/print.c
+++ lib/builtins/print.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void print(char * str){
   printf("%s\n",str);
   return;
 }
+
+/*
+void print_float(float dec){
+  printf("%f\n",dec);
+  return;
+}
+
+void print_int(int num){
+  printf("%d\n",num);
+  return;
+}
+
+void print_bool(bool truth_val){
+  printf("%s", truth_val ? "true" : "false");
+  return;
+}
+*/
