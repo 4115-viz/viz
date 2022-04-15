3ca5643dffb85a7010d5ec2ac242160515a5ec85
Yanhao Li <hi@yanhaoli.com>
2022-04-15 11:01:10 -0400

    fix: the container should be temporary

--- launch-container.sh
+++ launch-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # log into the the Viz Docker Container
-docker run -it -v $(pwd):/home/viz -w=/home/viz viz /bin/bash
+docker run --rm -it -v $(pwd):/home/viz -w=/home/viz viz /bin/bash
