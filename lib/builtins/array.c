c21d62e4c17381fc853c5dcd4079753d71b3fd19
nkw6 <nicholaskwu@gmail.com>
2022-05-07 19:54:50 -0400

    edited test files

--- lib/builtins/array.c
+++ lib/builtins/array.c
@@ -4,8 +4,12 @@
 
 /* casting from __ type to int */
 int array_len(int* arr){
+    int i = 0;
     printf("sizeof =%d\n", sizeof(arr)/sizeof(arr[0]));
-    printf("in array print name =%d, %d, %d, %d,%d\n", arr[0], arr[1], arr[2], arr[3], arr[4] );
+    printf("in array print =%d, %d, %d, %d,%d\n", arr[0], arr[1], arr[2], arr[3], arr[4] );
+    while(arr[i++] != 0){
+        printf("in array print arr = %d\n", arr[i - 1]);
+    }
     //name[3]= 55;
-    return 99;
+    return i;
 }
