1215188196bae12ec49bcc072008bba47179c323
Jakob Deutsch <jakobgabrield@aol.com>
2022-05-10 17:06:19 -0400

    Added two setup scripts

--- System_Setup_1.sh
+++ System_Setup_1.sh
@@ -15,13 +15,3 @@ sudo apt-get install -y bubblewrap
 opam init
 opam install dune
 eval $(opam env)
-
-# Install LLVM
-cd web-interface/functions/viz
-opam init --auto-setup --yes --disable-sandboxing
-chmod +x viz.opam
-opam install . --deps-only --yes
-sudo apt install llvm
-eval $(opam config env)
-dune clean
-dune build
